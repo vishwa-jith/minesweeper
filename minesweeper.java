cc833ad1221e8934b0f31dd68ffa35349d3bbd95
vishwajith <48879290+vishwa-jith@users.noreply.github.com>
2019-10-26 21:58:21 +0530

    minesweeper
    Minesweeper is a single-player puzzle computer game. The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field.

--- minesweeper.java
+++ minesweeper.java
@@ -15,50 +15,49 @@ implements ActionListener
 {
 	static char z[][]=new char[100][100];
     	static int n,m,n1,m1;
-	public boolean g;
     	public Button B[][]=new Button[100][100];
     	public static void input()
     	{
 		JFrame frame = new JFrame("..........WELCOME..........");
     		String name = JOptionPane.showInputDialog(frame, "1) Easy 2) Moderate 3) Difficult");
-		if(name.equals("1"))
+		if(name.equals("1"))	//Lvl 1
 		{
-			n=9;
-			n1=6;
-			m1=9;
-			m=9;
+			n=9;	//Number of rows
+			n1=6;	//2*Number of mines
+			m1=9;	//Unlocking factor
+			m=9;	//Number of columns
 		}
 		else if(name.equals("2"))
 		{
-			n=11;
-			n1=5;
-			m1=10;
-			m=11;
+			n=11;	//Number of rows
+			n1=5;	//Mine factor
+			m1=10;	//Unlocking factor
+			m=11;	//Number of columns
 		}
 		else if(name.equals("3"))
 		{
-			n=13;
-			n1=4;
-			m1=11;
-			m=13;
+			n=13;	//Number of rows
+			n1=4;	//Mine factor
+			m1=11;	//Unlocking factor
+			m=13;	//Number of columns
 		}
 		else
 		{
-			System.exit(0);
+			System.exit(0);	//Termination condition
 		}
 		for(int i=0;i<n;i++)
 		{
 			for(int j=0;j<m;j++)
 			{
-				z[i][j]='0';
+				z[i][j]='0';	//Initialization
 			}
 		}	
-        	Random rand1=new Random();
-        	for(int i=0;i<(n*m)/n1;i++)
-        	{
-            		int l= rand1.nextInt(n);
-            		int k= rand1.nextInt(m);
-            		z[l][k]='X';
+        	Random rand1=new Random();	
+        	for(int i=0;i<(n*m)/n1;i++)	
+        	{		
+            		int l= rand1.nextInt(n);	//Random row index
+            		int k= rand1.nextInt(m);	//Random column index
+            		z[l][k]='X';	//Setting mines
         	}
         	for(int i=0;i<n;i++)
         	{
@@ -66,7 +65,7 @@ implements ActionListener
             		{
                			try
                			{
-               				if(z[i][j]!='X')
+               				if(z[i][j]!='X')	//8 different cases for mine adjacency
                				{
                    				if(z[i-1][j]=='X')
                    				{
@@ -109,26 +108,25 @@ implements ActionListener
 	}
     	public void init()
     	{
-        	input();
-		g=false;
-		setLayout(new GridLayout(n,m));
+        	input();	//Setting mines at Random
+		setLayout(new GridLayout(n,m));	//Setting Layout
 		for(int i=0;i<n;i++)
         	{
 			for(int j=0;j<m;j++)
         		{
-		        	B[i][j]=new Button();
-		        	add(B[i][j]);
+		        	B[i][j]=new Button();	//Creation of button
+		        	add(B[i][j]);	//adding of button
                 		try
 				{
-                			B[i][j].setBackground(Color.orange);
-                			B[i][j].addActionListener(this);
+                			B[i][j].setBackground(Color.orange);	//Setting up background
+                			B[i][j].addActionListener(this);	//Setting up ActionListener
                 			if(z[i][j]=='X')
                 			{
-                				B[i][j].setActionCommand("Color.red");
+                				B[i][j].setActionCommand("Color.red");	//Setting ActionCommand for mines
                 			}
                 			else
                 			{
-                    				B[i][j].setActionCommand(Character.toString(z[i][j]));
+                    				B[i][j].setActionCommand(Character.toString(z[i][j])); //Setting ActionCommand other than mines
                 			}
             			}
             			catch(ArrayIndexOutOfBoundsException e)
@@ -139,31 +137,31 @@ implements ActionListener
     	public void actionPerformed(ActionEvent e)
     	{
 		Font f=new Font("Monotype Corsiva",Font.BOLD,40);
-		setFont(f);		
-		if(e.getActionCommand().equals("Color.red"))
+		setFont(f);	//Setting font
+		if(e.getActionCommand().equals("Color.red"))	//There exist a mine
         	{
             		for(int i=0;i<n;i++)
             		{
                 		for(int j=0;j<m;j++)
                 		{
-                    			if(B[i][j].getActionCommand().equals("Color.red"))
+                    			if(B[i][j].getActionCommand().equals("Color.red"))	//Finding all existing mines
                     			{
-                        			B[i][j].setBackground(Color.red);
+                        			B[i][j].setBackground(Color.red);	//Setting all mines to red color
                     			}
                 		}
             		}
         	}
-        	else if(e.getActionCommand().equals("0"))
+        	else if(e.getActionCommand().equals("0"))	//There exist no mines
         	{
 	    		int i,j,x=n/m1,k,l;
             		for(i=0;i<n;i++)
             		{
                 		for(j=0;j<m;j++)
                 		{
-					if(B[i][j]==e.getSource()) 
+					if(B[i][j]==e.getSource()) 	//Getting source button
 					{
-						B[i][j].setBackground(Color.green);
-						B[i][j].setLabel("0");
+						B[i][j].setBackground(Color.green);	//Setting current source to green color
+						B[i][j].setLabel("0");	
 						int c,d;
 						c=i-x;
 						d=j-x;
@@ -182,9 +180,9 @@ implements ActionListener
 	    					{
 							for(l=0;l<m;l++)
 							{
-								if(k>=c && k<=u && l>=d && l<=v)
+								if(k>=c && k<=u && l>=d && l<=v)	//Setting the range of elements to exist
 								{
-									if(B[k][l].getActionCommand().equals("0"))
+									if(B[k][l].getActionCommand().equals("0"))	
 									{		
 										B[k][l].setBackground(Color.green);
 										B[k][l].setLabel(B[k][l].getActionCommand());
@@ -201,7 +199,7 @@ implements ActionListener
                 		}
             		}
 		}
-	     	else
+	     	else	//There exist a adjacent mine
 	        {
 	     		for(int i=0;i<n;i++)
 	            	{
@@ -210,7 +208,8 @@ implements ActionListener
 	                    		if(e.getSource()==B[i][j])
         	            		{
 						String s=e.getActionCommand();
-						B[i][j].setBackground(Color.yellow);
+						B[i][j].setBackground(Color.yellow);	
+						//Setting all other elements except mines and no mines to yellow color
                         			B[i][j].setLabel(s);
 						break;
                 			}
